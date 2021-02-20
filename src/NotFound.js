bfabe2fbbf0b1c28abdc794607f424d5d1184bed
Luan Campos <luanlcampos15@gmail.com>
2021-02-19 21:55:05 -0500

    Not Found Style Added (Custom Page Missing)

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,8 +1,14 @@
+import {Container} from 'react-bootstrap';
+
+//Custom CSS
+import './NotFound.css';
+
 export default function NotFound() {
   return (
-    <div>
+    // <Container className="centerText flex-column align-middle text-center">
+    <Container className="centerText text-center">
       <h1>Not Found</h1>
       <p>We can't find what you're looking for...</p>
-    </div>
+    </Container>
   );
 }
